c4e869f30a031b8cda4909116729fb574af9dcdd
Boguslave <t.boltunov@g.nsu.ru>
2021-12-20 12:14:58 +0700

    all-1

---
+++ build/CMakeFiles/lab_1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_1.dir/func.cpp.obj"
+  "CMakeFiles/lab_1.dir/func.cpp.obj.d"
+  "CMakeFiles/lab_1.dir/main.cpp.obj"
+  "CMakeFiles/lab_1.dir/main.cpp.obj.d"
+  "lab_1.exe"
+  "lab_1.exe.manifest"
+  "lab_1.pdb"
+  "liblab_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
